29984cc5e1686c914ac5773968b7332ae6c979cd
julieta rainero <rainero.juli@gmail.com>
2024-10-31 18:09:10 -0300

    segunda entrega

--- js/main.js
+++ js/main.js
@@ -36,25 +36,53 @@ const productos = [
     new Producto("Guia de Alimentacion infantil", 20000),
     new Producto("Guia de Alimentacion durante el embarazo", 30000),
     new Producto("Guia de Alimentacion vegetarianismo", 15000),
-    new Producto("Guia de Alimentacion primer mes", 40000),
+    new Producto("Guia de Alimentacion mensual", 40000),
 ];
 
-
-function BuscarProducto(nombreBuscado) {
-    let encontrado = false;
-
-    for (let i = 0; i < productos.length; i++) {
-        if (productos[i].nombre.toLowerCase() === nombreBuscado.toLowerCase()) {
-            console.log("Producto encontrado: " + productos[i].nombre + ", Precio: " + productos[i].precio);
-            encontrado = true;
-            break;
-        }
-    }
-    if (!encontrado) {
-            console.log("Producto no encontrado");
-        }
-   
+function buscarProducto(termino) {
+    return productos.filter((producto) =>
+        producto.nombre.toLowerCase().includes(termino.toLowerCase())
+    );
 }
 
-const nombreBuscado = prompt("Ingrese el nombre del producto");
-BuscarProducto(nombreBuscado);
+const terminoBusqueda = prompt("Ingrese la palabra que desea buscar:");
+
+if (terminoBusqueda) {
+    const busqueda = buscarProducto(terminoBusqueda);
+
+    console.log(busqueda);
+
+    if (busqueda.length > 0) {
+        console.log(`Se encontraron ${busqueda.length} productos:`, busqueda);
+        
+        const nombresProductos = busqueda.map(producto => producto.nombre).join(", ");
+        alert("Se encontraron los siguientes productos: " + nombresProductos);
+    } else {
+        console.log("No se encontraron productos.");
+        alert("No se encontró el producto");
+    }
+} else {
+    console.log("No se ingresó ningún término de búsqueda.");
+}
+
+//carrusel de fotos
+let slideIndex = 0;
+
+function mostrarSlide(index) {
+    const slides = document.querySelectorAll('.carrusel-item');
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    } else {
+        slideIndex = index;
+    }
+
+    const carruselItems = document.querySelector('.carrusel-items');
+    carruselItems.style.transform = `translateX(${-slideIndex * 100}%)`;
+}
+
+function cambiarSlide(n) {
+    mostrarSlide(slideIndex + n);
+}
+
